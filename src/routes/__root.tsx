1ed126ec771a9699402e762f2b69e10d22bd74a7
mrprogrammer-afk <themelon.in@gmail.com>
2026-09-04 15:22:00 +0530

    style: change bride and groom names font to Dancing Script

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -115,7 +115,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300&family=Josefin+Sans:wght@300;400&family=Parisienne&family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300&family=Dancing+Script:wght@400;500;600;700&family=Josefin+Sans:wght@300;400&family=Parisienne&family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap",
       },
       {
         rel: "stylesheet",
